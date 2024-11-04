09004063be61fbf4773e89616c188ce76b5e0ae9
Afsal <afsalkp.mohammed@gmail.com>
2024-11-04 13:45:29 +0400

    Naming fixes

--- PlayGround/Solution.swift
+++ PlayGround/Solution.swift
@@ -10,11 +10,11 @@ import Foundation
 class Solution {
     
     // MARK: - Challenge 1: Are the letters unique?
-    func isUniqueItems(_ str: String) -> Bool {
+    func challenge1a(_ str: String) -> Bool {
         return Set(str).count == str.count
     }
     
-    func getUniqueItems(_ str: String) -> String {
+    func challenge1b(_ str: String) -> String {
         
         var dict = [String: Int]()
         
@@ -28,45 +28,45 @@ class Solution {
     }
     
     // MARK: - Challenge 2: Is a string a palindrome?
-    func isPalindrome(_ str: String) -> Bool {
+    func challenge2(_ str: String) -> Bool {
         return str.lowercased() == String(str.lowercased().reversed())
     }
     
     // MARK: - Challenge 3: Do two strings contain the same characters?
-    func sameCharacters(with str1: String, and str2: String) -> Bool {
+    func challenge3(with str1: String, and str2: String) -> Bool {
         return str1.sorted() == str2.sorted()
     }
     
     // MARK: - Challenge 4: Does one string contain another?
-    func containsWord(_ string: String, in sentence: String) -> Bool {
+    func challenge4(_ string: String, in sentence: String) -> Bool {
         return sentence.containsWord(string)
     }
     
     // MARK: - Challenge 5: Count the characters
-    func countCharacter(input: String, count: Character) -> Int {
+    func challenge5a(input: String, count: Character) -> Int {
         return input.reduce(0) {
             $1 == count ? $0 + 1 : $0
         }
     }
     
-    func countCharacterUsingFilter(input: String, count: Character) -> Int {
+    func challenge5b(input: String, count: Character) -> Int {
         return Array(input).filter { $0 == count }.count
     }
     
-    func countCharacterByRemoving(input: String, count: String) -> Int {
+    func challenge5c(input: String, count: String) -> Int {
         let modified = input.replacingOccurrences(of: count, with: "")
         return input.count - modified.count
     }
     
     // MARK: - Challenge 6: Remove duplicate letters from a string
-    func removeDuplicatesUsingNSOrderedSet(from string: String) -> String {
+    func challenge6a(from string: String) -> String {
         let array = string.map { String($0) }
         let set = NSOrderedSet(array: array)
         let letters = Array(set) as! Array<String>
         return letters.joined()
     }
     
-    func removeDuplicatesByLooping(from string: String) -> String {
+    func challenge6b(from string: String) -> String {
         var used = [Character]()
         for letter in string {
             if !used.contains(letter) {
@@ -77,7 +77,7 @@ class Solution {
         return String(used)
     }
     
-    func removeDuplicatesByFilter(from string: String) -> String {
+    func challenge6c(from string: String) -> String {
         var used = [Character: Bool]()
         
         let result = string.filter {
@@ -88,12 +88,12 @@ class Solution {
     }
     
     // MARK: - Challenge 7: Condense whitespace
-    func removeAdjacentSpaces(input: String) -> String {
+    func challenge7a(input: String) -> String {
         let components = input.components(separatedBy: .whitespacesAndNewlines)
         return components.filter { !$0.isEmpty }.joined(separator: " ")
     }
     
-    func retainSingleSpace(input: String) -> String {
+    func challenge7b(input: String) -> String {
         var seenSpace = false
         var returnValue = ""
         
@@ -111,19 +111,19 @@ class Solution {
         return returnValue
     }
     
-    func singleSpaceByRegEx(input: String) -> String {
+    func challenge7c(input: String) -> String {
         return input.replacingOccurrences(of: " +", with: " ", options: .regularExpression, range: nil)
     }
     
     // MARK: - Challenge 8: String is rotated
-    func rotateString(input: String, rotated: String) -> Bool {
+    func challenge8(input: String, rotated: String) -> Bool {
         guard input.count == rotated.count else { return false }
         let combined = input + input
         return combined.contains(rotated)
     }
     
     // MARK: - Challenge 9: Find pangrams
-    func isPangrams(input: String) -> Bool {
+    func challenge9(input: String) -> Bool {
         let set = Set(input.lowercased())
         let letters = set.filter { $0 >= "a" && $0 <= "z" }
         return letters.count == 26
--- PlayGroundTests/SoutionTests.swift
+++ PlayGroundTests/SoutionTests.swift
@@ -12,199 +12,199 @@ class SoutionTests: XCTestCase {
     
     // MARK: - Challenge 1: Are the letters unique?
     func test_isUniqueItems_returnTrue() {
-        let result = makeSUT().isUniqueItems("No duplicates")
+        let result = makeSUT().challenge1a("No duplicates")
         XCTAssertTrue(result)
         
-        let result2 = makeSUT().isUniqueItems("abcdefghijklmnopqrstuvwxyz")
+        let result2 = makeSUT().challenge1a("abcdefghijklmnopqrstuvwxyz")
         XCTAssertTrue(result2)
         
-        let result3 = makeSUT().isUniqueItems("AaBbCc")
+        let result3 = makeSUT().challenge1a("AaBbCc")
         XCTAssertTrue(result3)
         
-        let result4 = makeSUT().isUniqueItems("Hello, world")
+        let result4 = makeSUT().challenge1a("Hello, world")
         XCTAssertFalse(result4)
     }
     
     func test_uniqueItemsString_returnUniqueItems() {
-        let result = makeSUT().getUniqueItems("Aaaabbbcessaabrrnnnq")
+        let result = makeSUT().challenge1b("Aaaabbbcessaabrrnnnq")
         XCTAssertEqual(result, "ceq")
     }
     
     // MARK: - Challenge 2: Is a string a palindrome?
     func test_isPalindrome_returnTrue() {
-        let result = makeSUT().isPalindrome("rotator")
+        let result = makeSUT().challenge2("rotator")
         XCTAssertTrue(result)
         
-        let result2 = makeSUT().isPalindrome("Rats live on no evil star")
+        let result2 = makeSUT().challenge2("Rats live on no evil star")
         XCTAssertTrue(result2)
         
-        let result3 = makeSUT().isPalindrome("Never odd or even")
+        let result3 = makeSUT().challenge2("Never odd or even")
         XCTAssertFalse(result3)
         
-        let result4 = makeSUT().isPalindrome("Hello, world")
+        let result4 = makeSUT().challenge2("Hello, world")
         XCTAssertFalse(result4)
     }
     
     // MARK: - Challenge 3: Do two strings contain the same characters?
     func test_sameCharacters_returnTrue() {
-        let result = makeSUT().sameCharacters(with: "abca", and: "abca")
+        let result = makeSUT().challenge3(with: "abca", and: "abca")
         XCTAssertTrue(result)
         
-        let result2 = makeSUT().sameCharacters(with: "abc", and: "cba")
+        let result2 = makeSUT().challenge3(with: "abc", and: "cba")
         XCTAssertTrue(result2)
         
-        let result3 = makeSUT().sameCharacters(with: "a1 b2", and: "b1 a2")
+        let result3 = makeSUT().challenge3(with: "a1 b2", and: "b1 a2")
         XCTAssertTrue(result3)
         
-        let result4 = makeSUT().sameCharacters(with: "abc", and: "abca")
+        let result4 = makeSUT().challenge3(with: "abc", and: "abca")
         XCTAssertFalse(result4)
         
-        let result5 = makeSUT().sameCharacters(with: "abc", and: "Abc")
+        let result5 = makeSUT().challenge3(with: "abc", and: "Abc")
         XCTAssertFalse(result5)
         
-        let result6 = makeSUT().sameCharacters(with: "abc", and: "cbAa")
+        let result6 = makeSUT().challenge3(with: "abc", and: "cbAa")
         XCTAssertFalse(result6)
         
-        let result7 = makeSUT().sameCharacters(with: "abcc", and: "abca")
+        let result7 = makeSUT().challenge3(with: "abcc", and: "abca")
         XCTAssertFalse(result7)
     }
     
     // MARK: - Challenge 4: Does one string contain another?
     func test_containsWord_returnsBoolean() {
-        let result = makeSUT().containsWord("Hello", in: "Hello, world")
+        let result = makeSUT().challenge4("Hello", in: "Hello, world")
         XCTAssertTrue(result)
         
-        let result2 = makeSUT().containsWord("WORLD", in: "Hello, world")
+        let result2 = makeSUT().challenge4("WORLD", in: "Hello, world")
         XCTAssertTrue(result2)
         
-        let result3 = makeSUT().containsWord("Goodbye", in: "Hello, world")
+        let result3 = makeSUT().challenge4("Goodbye", in: "Hello, world")
         XCTAssertFalse(result3)
     }
     
     // MARK: - Challenge 5: Count the characters
     func test_countCharacter_returnsTheCount() {
-        let result = makeSUT().countCharacter(input: "The rain in Spain", count: "a")
+        let result = makeSUT().challenge5a(input: "The rain in Spain", count: "a")
         XCTAssertEqual(result, 2)
         
-        let result2 = makeSUT().countCharacter(input: "Mississippi", count: "i")
+        let result2 = makeSUT().challenge5a(input: "Mississippi", count: "i")
         XCTAssertEqual(result2, 4)
         
-        let result3 = makeSUT().countCharacter(input: "Hacking with Swift", count: "i")
+        let result3 = makeSUT().challenge5a(input: "Hacking with Swift", count: "i")
         XCTAssertEqual(result3, 3)
     }
         
     func test_countCharacterUsingFilter_returnsTheCount() {
-        let result = makeSUT().countCharacterUsingFilter(input: "The rain in Spain", count: "a")
+        let result = makeSUT().challenge5b(input: "The rain in Spain", count: "a")
         XCTAssertEqual(result, 2)
         
-        let result2 = makeSUT().countCharacterUsingFilter(input: "Mississippi", count: "i")
+        let result2 = makeSUT().challenge5b(input: "Mississippi", count: "i")
         XCTAssertEqual(result2, 4)
         
-        let result3 = makeSUT().countCharacterUsingFilter(input: "Hacking with Swift", count: "i")
+        let result3 = makeSUT().challenge5b(input: "Hacking with Swift", count: "i")
         XCTAssertEqual(result3, 3)
     }
         
     func test_countCharacterByRemoving_returnsTheCount() {
-        let result = makeSUT().countCharacterByRemoving(input: "The rain in Spain", count: "a")
+        let result = makeSUT().challenge5c(input: "The rain in Spain", count: "a")
         XCTAssertEqual(result, 2)
         
-        let result2 = makeSUT().countCharacterByRemoving(input: "Mississippi", count: "i")
+        let result2 = makeSUT().challenge5c(input: "Mississippi", count: "i")
         XCTAssertEqual(result2, 4)
         
-        let result3 = makeSUT().countCharacterByRemoving(input: "Hacking with Swift", count: "i")
+        let result3 = makeSUT().challenge5c(input: "Hacking with Swift", count: "i")
         XCTAssertEqual(result3, 3)
     }
         
     // MARK: - Challenge 6: Remove duplicate letters from a string
     func test_removeDuplicatesUsingNSOrderedSet_givesString() {
-        let result = makeSUT().removeDuplicatesUsingNSOrderedSet(from: "wombat")
+        let result = makeSUT().challenge6a(from: "wombat")
         XCTAssertEqual(result, "wombat")
         
-        let result2 = makeSUT().removeDuplicatesUsingNSOrderedSet(from: "hello")
+        let result2 = makeSUT().challenge6a(from: "hello")
         XCTAssertEqual(result2, "helo")
         
-        let result3 = makeSUT().removeDuplicatesUsingNSOrderedSet(from: "Mississippi")
+        let result3 = makeSUT().challenge6a(from: "Mississippi")
         XCTAssertEqual(result3, "Misp")
     }
     
     func test_removeDuplicatesByLooping_givesString() {
-        let result = makeSUT().removeDuplicatesByLooping(from: "wombat")
+        let result = makeSUT().challenge6b(from: "wombat")
         XCTAssertEqual(result, "wombat")
         
-        let result2 = makeSUT().removeDuplicatesByLooping(from: "hello")
+        let result2 = makeSUT().challenge6b(from: "hello")
         XCTAssertEqual(result2, "helo")
         
-        let result3 = makeSUT().removeDuplicatesByLooping(from: "Mississippi")
+        let result3 = makeSUT().challenge6b(from: "Mississippi")
         XCTAssertEqual(result3, "Misp")
     }
     
     func test_removeDuplicatesByFilter_givesString() {
-        let result = makeSUT().removeDuplicatesByFilter(from: "wombat")
+        let result = makeSUT().challenge6c(from: "wombat")
         XCTAssertEqual(result, "wombat")
         
-        let result2 = makeSUT().removeDuplicatesByFilter(from: "hello")
+        let result2 = makeSUT().challenge6c(from: "hello")
         XCTAssertEqual(result2, "helo")
         
-        let result3 = makeSUT().removeDuplicatesByFilter(from: "Mississippi")
+        let result3 = makeSUT().challenge6c(from: "Mississippi")
         XCTAssertEqual(result3, "Misp")
     }
     
     // MARK: - Challenge 7: Condense whitespace
     func test_removeAdjacentSpaces_returnValue() {
-        let result = makeSUT().removeAdjacentSpaces(input: "a   b   c")
+        let result = makeSUT().challenge7a(input: "a   b   c")
         XCTAssertEqual(result, "a b c")
         
-        let result2 = makeSUT().removeAdjacentSpaces(input: "    a")
+        let result2 = makeSUT().challenge7a(input: "    a")
         XCTAssertEqual(result2, "a")
         
-        let result3 = makeSUT().removeAdjacentSpaces(input: " abc ")
+        let result3 = makeSUT().challenge7a(input: " abc ")
         XCTAssertEqual(result3, "abc")
     }
     
     func test_retainSingleSpace_returnValue() {
-        let result = makeSUT().retainSingleSpace(input: "a   b   c")
+        let result = makeSUT().challenge7b(input: "a   b   c")
         XCTAssertEqual(result, "a b c")
         
-        let result2 = makeSUT().retainSingleSpace(input: "    a")
+        let result2 = makeSUT().challenge7b(input: "    a")
         XCTAssertEqual(result2, " a")
         
-        let result3 = makeSUT().retainSingleSpace(input: "abc")
+        let result3 = makeSUT().challenge7b(input: "abc")
         XCTAssertEqual(result3, "abc")
     }
     
     func test_singleSpaceByRegEx_returnValue() {
-        let result = makeSUT().singleSpaceByRegEx(input: "a   b   c")
+        let result = makeSUT().challenge7c(input: "a   b   c")
         XCTAssertEqual(result, "a b c")
         
-        let result2 = makeSUT().singleSpaceByRegEx(input: "    a")
+        let result2 = makeSUT().challenge7c(input: "    a")
         XCTAssertEqual(result2, " a")
         
-        let result3 = makeSUT().singleSpaceByRegEx(input: "abc")
+        let result3 = makeSUT().challenge7c(input: "abc")
         XCTAssertEqual(result3, "abc")
     }
     
     // MARK: - Challenge 8: String is rotated
     func test_rotateString_givesRotated() {
-        let result = makeSUT().rotateString(input: "abcde", rotated: "eabcd")
+        let result = makeSUT().challenge8(input: "abcde", rotated: "eabcd")
         XCTAssertTrue(result)
         
-        let result2 = makeSUT().rotateString(input: "abcde", rotated: "cdeab")
+        let result2 = makeSUT().challenge8(input: "abcde", rotated: "cdeab")
         XCTAssertTrue(result2)
         
-        let result3 = makeSUT().rotateString(input: "abcde", rotated: "abced")
+        let result3 = makeSUT().challenge8(input: "abcde", rotated: "abced")
         XCTAssertFalse(result3)
         
-        let result4 = makeSUT().rotateString(input: "abc", rotated: "a")
+        let result4 = makeSUT().challenge8(input: "abc", rotated: "a")
         XCTAssertFalse(result4)
     }
     
     // MARK: - Challenge 9: Find pangrams
     func test_isPangrams_returnBoolean() {
-        let result = makeSUT().isPangrams(input: "The quick brown fox jumps over the lazy dog")
-        XCTAssertTrue(result, "the panagram should return true")
+        let result = makeSUT().challenge9(input: "The quick brown fox jumps over the lazy dog")
+        XCTAssertTrue(result)
         
-        let result2 = makeSUT().isPangrams(input: "The quick brown fox jumped over the lazy dog")
-        XCTAssertFalse(result2, "the panagram should return false")
+        let result2 = makeSUT().challenge9(input: "The quick brown fox jumped over the lazy dog")
+        XCTAssertFalse(result2)
     }
     
     // MARK: - Helpers
